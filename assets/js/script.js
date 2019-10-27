001bfad54b8cbb7c96451accf8c25cebe948ac4c
jhars <jonharlan.io@gmail.com>
2019-10-27 15:47:19 -0400

    comment out 360 loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,34 +167,6 @@ $(document).ready(function () {
 
     });
 
-
-
-
-
-
-
-    // var mapCanvas = document.getElementById('map-canvas');
-    // var mapOptions = {
-    //     center: new google.maps.LatLng(24.909439, 91.833800),
-    //     zoom: 16,
-    //     scrollwheel: false,
-    //     mapTypeId: google.maps.MapTypeId.ROADMAP
-    // }
-    // var map = new google.maps.Map(mapCanvas, mapOptions)
-
-    // var marker = new google.maps.Marker({
-    //         position: new google.maps.LatLng(24.909439, 91.833800),
-    //         title:"Boots4 Office"
-    //     });
-
-        // To add the marker to the map, call setMap();
-        // marker.setMap(map);
-
-    //google.maps.event.addDomListener(window, 'load', initialize);
-
-
-    // Show Reletive Page Onclick
-
     $('.menu div.profile-btn').on('click', function () {
         $('.profile-page').fadeIn(1200);
         setTimeout(function(){
