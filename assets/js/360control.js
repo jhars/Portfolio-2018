4d45eea8086445d04d3543e6a966c52ef5565067
jhars <jonharlan.io@gmail.com>
2019-10-27 16:12:03 -0400

    this seems to work...

--- assets/js/360control.js
+++ assets/js/360control.js
@@ -1,70 +1,70 @@
-// // window.addEventListener('load', initControl, false);
-//
-// var canvas = document.getElementById("clickCanvas");
-// var ctx = canvas.getContext("2d");
-//
+// window.addEventListener('load', initControl, false);
+
+var canvas = document.getElementById("clickCanvas");
+var ctx = canvas.getContext("2d");
+
 // var image = new Image()
 // image.src = 'http://i.imgur.com/BfY4QW5.png';
-//
-// var frames = 100;
-// var bottomFrame = 18;
-// var currentFrame;
-// var currentFrame = 18;
-// var click = false;
-// var width = 109;
-// var height = 81.75;
-// var B;
-// var ALPHA;
-// var DIFF = 0;
+
+var frames = 100;
+var bottomFrame = 18;
+var currentFrame;
+var currentFrame = 18;
+var click = false;
+var width = 109;
+var height = 81.75;
+var B;
+var ALPHA;
+var DIFF = 0;
 // ctx.drawImage(image, 0, height * currentFrame, width, height, 0, 0, width, height);
-//
-// function initControl() {
-// 	var X = [];
-// 	var DELTA;
-//
-// 	canvas.addEventListener("mousedown", doMouseDown, false);
-// 	canvas.addEventListener("touchstart", doMouseDown, false);
-//
-// 	function doMouseDown (event) {
-// 		X[0] = event.pageX;
-//
-// 		canvas.addEventListener("touchmove", mouseX, true);
-// 		canvas.addEventListener("touchend", mouseUp, false);
-//
-// 		canvas.addEventListener("mousemove",mouseX, false);
-// 		document.body.addEventListener("mouseup", mouseUp, false);
-//
-// 	};
-//
-// 	function mouseUp(event) {
-// 	  canvas.removeEventListener("mousemove",mouseX, false);
-// 	  canvas.addEventListener("touchmove", mouseX, true);
-// 	};
-//
-// 	function mouseX (event) {
-// 	    var canvasX = event.pageX;
-// 	    X[1] = canvasX;
-// 	  	A = ALPHA; DIFF = X[0] - X[1]; ALPHA = DIFF; B = ALPHA;
-//
-// 			if (B < A){//dragging RIGHT
-// 	  		if (currentFrame === frames-1 ) {
-// 	  			currentFrame = frames-1;//>> Limit Here (upper)
-// 	  		} else {
-// 	  			currentFrame++;
-// 	  		}
-// 	  	}
-// 	  	if (B > A) {//dragging LEFT
-// 	  		if (currentFrame < bottomFrame ) {//or TRY 1 here...> see helmetLoop
-// 	  			currentFrame = 18;//>> Limit Here (lower)
-// 	  		}
-// 	  		currentFrame--;
-// 	  	}
-//
-// 	  	var ALPHA2 = ALPHA;
-// 	  	ctx.clearRect(0, 0, width, height);
-// 	  	ctx.drawImage(image, 0, height * currentFrame, width, height, 0, 0, width, height);
-// 	  	X[1] = X[0];
-//
-// 	}
-//
-// }
+
+function initControl() {
+	// var X = [];
+	// var DELTA;
+  //
+	// canvas.addEventListener("mousedown", doMouseDown, false);
+	// canvas.addEventListener("touchstart", doMouseDown, false);
+
+	// function doMouseDown (event) {
+	// 	X[0] = event.pageX;
+  //
+	// 	canvas.addEventListener("touchmove", mouseX, true);
+	// 	canvas.addEventListener("touchend", mouseUp, false);
+  //
+	// 	canvas.addEventListener("mousemove",mouseX, false);
+	// 	document.body.addEventListener("mouseup", mouseUp, false);
+  //
+	// };
+
+	// function mouseUp(event) {
+	//   canvas.removeEventListener("mousemove",mouseX, false);
+	//   canvas.addEventListener("touchmove", mouseX, true);
+	// };
+
+	// function mouseX (event) {
+	//     var canvasX = event.pageX;
+	//     X[1] = canvasX;
+	//   	A = ALPHA; DIFF = X[0] - X[1]; ALPHA = DIFF; B = ALPHA;
+  //
+	// 		if (B < A){//dragging RIGHT
+	//   		if (currentFrame === frames-1 ) {
+	//   			currentFrame = frames-1;//>> Limit Here (upper)
+	//   		} else {
+	//   			currentFrame++;
+	//   		}
+	//   	}
+	//   	if (B > A) {//dragging LEFT
+	//   		if (currentFrame < bottomFrame ) {//or TRY 1 here...> see helmetLoop
+	//   			currentFrame = 18;//>> Limit Here (lower)
+	//   		}
+	//   		currentFrame--;
+	//   	}
+  //
+	//   	var ALPHA2 = ALPHA;
+	//   	ctx.clearRect(0, 0, width, height);
+	//   	ctx.drawImage(image, 0, height * currentFrame, width, height, 0, 0, width, height);
+	//   	X[1] = X[0];
+  //
+	// }
+
+}
